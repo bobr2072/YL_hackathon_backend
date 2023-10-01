71498647c7c45fa2ee432ff09e5ab497fe408722
bobr <borya2072@gmail.com>
2023-10-01 17:37:14 +0300

    trying new models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,9 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, viewsets
+from rest_framework import filters, viewsets
 
 from api.models import Categories, Forecast, Sales, Stores
-from api.serializers import (CategoriesSerializer, ForecastGetSerializer,
-                             ForecastPostSerializer, SalesSerializer,
+from api.serializers import (CategoriesSerializer,
+                             ForecastSerializer, SalesSerializer,
                              ShopsSerializer)
 
 
@@ -18,7 +18,7 @@ class SalesViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Sales.objects.all()
     serializer_class = SalesSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['saled_product', 'store']
+    filterset_fields = ['product', 'store']
 
 
 class StoresViewSet(viewsets.ReadOnlyModelViewSet):
@@ -28,11 +28,4 @@ class StoresViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ForecastViewSet(viewsets.ModelViewSet):
     queryset = Forecast.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method in permissions.SAFE_METHODS:
-            return ForecastGetSerializer
-        return ForecastPostSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(product=self.request.POST.get('predictions.product'))
+    serializer_class = ForecastSerializer
