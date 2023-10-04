bfcd3254de7d04d5ffaf2e62c463de8ba2491dac
Anastasia7Si <120373288+Anastasia7Si@users.noreply.github.com>
2023-10-04 19:55:39 +0300

    rm dir frontend

--- frontend/src/components/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import initialReducer from "./initialState";
-
-export const store = configureStore({
-  reducer: {
-    initial: initialReducer,
-  }
-});
